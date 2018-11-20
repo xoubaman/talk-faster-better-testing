2127ab28ad4593592d94c5a8ae4888bdc8b74048
Carlos Gándara <carlos.gandara@docplanner.com>
2018-11-20 13:31:48 +0100

    Add some php code for IntelliJ examples

---
+++ examples/MyClass.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Example;
+
+class MyClass
+{
+    public function returnABoolean(): bool
+    {
+        return true;
+    }
+
+    public function returnAString(): string
+    {
+        return "Hello darkness my old friend";
+    }
+}
---
+++ examples/tests/MyClassTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Example\MyClass;
+use PHPUnit\Framework\TestCase;
+
+class MyClassTest extends TestCase
+{
+    public function testReturnsTrue(): void
+    {
+        $class = new MyClass();
+
+        self::assertTrue($class->returnABoolean());
+    }
+
+    public function testReturnsString(): void
+    {
+        $class = new MyClass();
+
+        self::assertSame(
+            "Hello darkness my old friend",
+            $class->returnAString()
+        );
+    }
+}
